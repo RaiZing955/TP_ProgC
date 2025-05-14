c285f48353a2c4144e513ac6ce73df35d954c216
RaiZing955 <d-kaya@outlook.fr>
2025-05-14 17:39:59 +0200

    Update serveur.h

--- TP5/src/serveur.h
+++ TP5/src/serveur.h
@@ -1,7 +1,19 @@
-#ifndef SERVEUR_H
-#define SERVEUR_H
+/*
+ * SPDX-FileCopyrightText: 2021 John Samuel
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
 
-void lance_serveur();
-void recois_envoie_message(int socket_client);
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+#define PORT 8089
+
+/* accepter la nouvelle connection d'un client et lire les données
+ * envoyées par le client. En suite, le serveur envoie un message
+ * en retour
+ */
+int renvoie_message(int, char *);
 
 #endif
